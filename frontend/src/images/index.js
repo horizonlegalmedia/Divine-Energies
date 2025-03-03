c93419045aab70fd2b9359d9294981eab5755298
horizonlegalmedia <horizonlegalmedia@gmail.com>
2025-03-03 16:44:40 +0530

    changed image import

--- frontend/src/images/index.js
+++ frontend/src/images/index.js
@@ -1,7 +1,7 @@
 export { default as manifestation } from './manifestation.jpg';
 export { default as manifestationCoaching } from './manifestationCoaching.jpg';
 export { default as affirmationAndPositiveThinking } from './affirmationAndPositiveThinking.jpg';
-export { default as mindsetMastery } from './mindsetmastery.png';
+export { default as mindsetMastery } from './mindsetMastery.png';
 export { default as owner } from './owner.jpg';
 export { default as qr } from './qr.png';
 export { default as logo } from './divineEnergies.jpg';
